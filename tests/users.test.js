3f8baebad2da410591a5c2a53eb6493ea78474df
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-02 00:24:27 +0200

    Update user test file to use correct app path and enhance user data for signup tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
-const app = require("./app-test");
+const app = require("../app-tours"); // Your Express app
 const api = supertest(app);
 const User = require("../models/userModel");
 
@@ -14,9 +14,13 @@ describe('User Routes', () => {
     it('should signup a new user with valid credentials', async () => {
       // Arrange
       const userData = {
+        name: "John Doe",
         email: 'test@example.com',
-        password: 'R3g5T7#gh',
-        name:"sami"
+        password: 'S5g5T7!et',
+        phone_number: '1234567890',
+        gender: 'male',
+        date_of_birth: '1990-01-01',
+        membership_status: 'inactive'
       };
 
       // Act
@@ -52,7 +56,7 @@ describe('User Routes', () => {
       // Arrange
       const userData = {
         email: 'test@example.com',
-        password: 'R3g5T7#gh'
+        password: 'S5g5T7!et'
       };
 
       // Act
